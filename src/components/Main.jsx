6bb5fc6840065de2df6ec50abfb9a06b8d65ebea
Ejimoforanthony <ejimoforanthony@yahoo.com>
2024-02-21 16:47:31 +0100

    project

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainLeft from './MainLeft';
+import MainRight from './MainRight';
+
+const Main = () => {
+  return (
+    <div className='main'>
+     <MainRight/>
+     <MainLeft/>
+    </div>
+  )
+}
+
+export default Main
