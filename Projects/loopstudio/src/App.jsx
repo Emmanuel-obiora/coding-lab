ae0f322d9177664b36f7dfc24b3bea19ddf706a8
Ejimoforanthony <ejimoforanthony@yahoo.com>
2024-02-23 19:10:41 +0100

    update

---
+++ Projects/loopstudio/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Components/Header'
+import Main from './Components/Main/Main'
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <div>
+        <Header />
+        <Main />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
